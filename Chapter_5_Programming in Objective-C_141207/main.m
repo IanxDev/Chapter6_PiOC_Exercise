5d61e5025f8954e70d2e6b25245414c9d0915675
Ian Wu <Verinda@IandeMacBook-Pro.local>
2014-12-07 22:05:53 +0800

    Initial Commit

---
+++ Chapter_5_Programming in Objective-C_141207/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Chapter_5_Programming in Objective-C_141207
+//
+//  Created by Ian Wu on 14/12/7.
+//  Copyright (c) 2014年 Ian. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
